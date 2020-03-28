21c2b3c2c02beb2b442c79e692a6b4c38653e669
adefirmanf <syskeys32@gmail.com>
2020-03-28 19:49:56 +0700

    add poolEnd

---
+++ db/index.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "postgres",
+  port: 5432
+})
+
+module.exports = {
+  query: async (text, params) => {
+    try {
+      return await pool.query(text, params)
+    }
+    catch (err) {
+      throw new Error(err)
+    }
+  },
+
+  // Useful for graceful shutdown
+  poolEnd: async () => {
+    pool.end().then(() => console.log("pool has ended"))
+  }
+}
