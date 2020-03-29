83b25301fa1b45177f2e85cb133ab7acbcf70f52
adefirmanf <syskeys32@gmail.com>
2020-03-29 14:46:16 +0700

    update controllername

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const messageControllers = require('../controllers/messageControllers')
 
 // Get all message
-router.get("/", messageControllers.MESSAGE_GET)
+router.get("/", messageControllers.MESSAGE_GET_ALL)
 
 // Get all message by id
 router.get("/:id", messageControllers.MESSAGE_GET_BY_ID)
