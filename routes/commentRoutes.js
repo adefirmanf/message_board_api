ec9efa862c9262a57a9181ae0497f1ae68efb5b2
adefirmanf <syskeys32@gmail.com>
2020-03-29 14:49:17 +0700

    change params name to message_id

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router()
 const commentControllers = require('../controllers/commentControllers')
 
 // Get all comment
-router.get("/", commentControllers.COMMENT_GET)
+router.get("/", commentControllers.COMMENT_GET_BY_ALL)
 
 // Get all comment by message id
-router.get("/message/:id", commentControllers.COMMENT_GET_BY_MESSAGE_ID)
+router.get("/message/:message_id", commentControllers.COMMENT_GET_BY_MESSAGE_ID)
 
 // Post comment by message id
-router.post("/message/:id", commentControllers.COMMENT_POST_BY_MESSAGE_ID)
+router.post("/message/:message_id", commentControllers.COMMENT_POST_BY_MESSAGE_ID)
 
 
 module.exports = router;
